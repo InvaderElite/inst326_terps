555ec010eadb2827c2628629532e31246af8aef5
Brandon Dang <bdang5115@gmail.com>
2022-11-18 00:07:34 -0500

    changed tank character and added mage

--- inst326_terps.py
+++ inst326_terps.py
@@ -1,5 +1,5 @@
 class BaseCharacter:
-    """Standard character with default status
+    """Standard character with default status and actions
     
     Attributes:
         name (str): name of character
@@ -8,14 +8,13 @@ class BaseCharacter:
         defense (int): characters level of defense
         
     """
-    def __init__(self,):
+    def __init__(self, name):
         """Creates the character to be played on the field
         
         Args:
             name (str): name of character
             
         """
-        
         
     def attack(self, opponent):
         """Move to deal damage to an opponent.
@@ -27,18 +26,46 @@ class BaseCharacter:
             Writes to stdout that opponent has taken damage
         """
         
-    
-    def defend(self):
-        """Move to defend against possible incoming damage, lasts one turn
-        
-        Side effects:
-            Writes to stdout that character has defended for the round
-        """
         
 class Tank(BaseCharacter):
-    """Tank class variation of base character, has more health and defense, less
-    power
+    """Tank class variation of base character, has more health, less
+    power. Has an additional defense ability.
+    
+    Attributes:
+
     """
-    def __init__(self, ):
-        """Sets the status of the tank character
+    def __init__(self, name):
+        """Sets the status of the tank character, uses the base character stats
+        but with more health and less power.
+        
+        Args:
+            name (str): name of character
+        """
+        
+    def defend(self):
+        """Action to defend against possible incoming damage, lasts one turn,
+        would add temporary health to the character 
+           
+        Side effects:
+            Writes to stdout that character has defended for the round, and adds
+            health to character
+        """
+        
+class Mage(BaseCharacter):
+    """Magic caster variation of base character, but has more power, and less
+    health. Has no standard attack action, but two casting actions.
+    
+    Attributes:
+
+    """
+    def __init__(self):
+        """Sets the status of the tank character, uses the base character stats
+        but with more power and less health.
+        
+        Args:
+            name (str): name of character
+        """
+        
+    def fireball(self, opponent):
+        """Action to attack 
         """
